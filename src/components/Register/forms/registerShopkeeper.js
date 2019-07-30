cad78936da7b34a2363b4268eda30e721e58bad9
ggglyke <guillaluckyluke@hotmail.fr>
2019-07-30 13:01:43 +0200

    formulaires inscription dynamiques

---
+++ src/components/Register/forms/registerShopkeeper.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Input from 'components/Forms';
+
+const RegisterShopkeeper = () => (
+  <>
+    <h3 className="topSectionH3 mt-5">Votre commerce</h3>
+    <Input
+      type="text"
+      label="Nom de votre commerce"
+      name="shopName"
+      id="shopName"
+      placeholder="ex: Café de la gare"
+      required={true}
+    />
+    <div class="form-group">
+      <label htmlFor="shopCategory">Catégorie de l'établissement</label>
+      <select class="form-control" id="shopCategory" name="shopCategory">
+        <option value="cafe">Café</option>
+        <option value="restaurant">Restaurant</option>
+      </select>
+    </div>
+    <Input
+      type="text"
+      label="Adresse votre commerce"
+      name="shopAddress"
+      id="shopAddress"
+      placeholder="ex: 15 rue des Lilas 75018 Paris"
+      required={true}
+    />
+  </>
+);
+
+export default RegisterShopkeeper;
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -2,132 +2,129 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import Header from 'components/Header';
+import Input from 'components/Forms';
+import RegisterShopkeeper from './forms/registerShopkeeper';
 import './register.scss';
 
 class Register extends React.Component {
-    state = {
+  state = {
     roleTitle: '',
     role: '',
-  }
+  };
 
   componentDidMount = () => {
     console.log('state', this.props.location.state);
-    const { roleTitle, role } =  this.props.location.state;
+    const { roleTitle, role } = this.props.location.state;
     //console.log(roleTitle, role);
     this.setState({
       roleTitle: roleTitle,
       role: role,
     });
-  }
+  };
 
   render() {
+    const role = this.state.role;
     return (
       <>
         <Header title={`Inscription ${this.state.roleTitle}`} />
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <form className="mt-4">
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="registerRoleSelector"
-                  id="inlineRadio1"
-                  value="donor"
-                  checked={this.state.role === 'donor' ? 'checked' : ''}
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-12 col-lg-6">
+              <form>
+                {// if no role in props, display role selector
+                role === 'donor' || role === 'beneficiary' || role === 'shopkeeper' ? (
+                  ''
+                ) : (
+                  <div className="radios">
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="registerRoleSelector"
+                        id="inlineRadio1"
+                        value="donor"
+                        checked={this.state.role === 'donor' ? 'checked' : ''}
+                      />
+                      <label className="form-check-label" htmlFor="inlineRadio1">
+                        Donateur
+                      </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="registerRoleSelector"
+                        id="inlineRadio2"
+                        value="beneficiary"
+                        checked={this.state.role === 'beneficiary' ? 'checked' : ''}
+                      />
+                      <label className="form-check-label" htmlFor="inlineRadio2">
+                        Bénéficaire
+                      </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="registerRoleSelector"
+                        id="inlineRadio2"
+                        value="shopkeeper"
+                        checked={this.state.role === 'shopkeeper' ? 'checked' : ''}
+                      />
+                      <label className="form-check-label" htmlFor="inlineRadio2">
+                        Commerçant
+                      </label>
+                    </div>
+                  </div>
+                )}
+                <h3>Vos identifiants</h3>
+                <Input
+                  type="text"
+                  label="Nom d'utilisateur"
+                  name="username"
+                  id="username"
+                  placeholder="ex: Tintin72"
+                  required={true}
                 />
-                <label className="form-check-label" for="inlineRadio1">
-                  Donateur
-                </label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="registerRoleSelector"
-                  id="inlineRadio2"
-                  value="beneficiary"
-                  checked={this.state.role === 'beneficiary' ? 'checked' : ''}
-                />
-                <label className="form-check-label" for="inlineRadio2">
-                  Bénéficaire
-                </label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="registerRoleSelector"
-                  id="inlineRadio2"
-                  value="shopkeeper"
-                  checked={this.state.role === 'shopkeeper' ? 'checked' : ''}
-                />
-                <label className="form-check-label" for="inlineRadio2">
-                  Commerçant
-                </label>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <form className="mt-4">
-              <div className="form-group">
-                <label for="exampleInputUserName1">Votre nom d'utilisateur</label>
-                <input
-                  type="userName"
-                  className="form-control"
-                  id="exampleInputUserName1"
-                  placeholder="Entrez votre nom d'utilisateur"
-                />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputEmail1">Votre adresse email</label>
-                <input
+                <Input
                   type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Entrez votre adresse e-mail"
+                  label="Adresse email"
+                  name="email"
+                  id="email"
+                  placeholder="mon-mail@service.com"
+                  required={role !== 'beneficiary' ? true : false}
                 />
-              </div>
-
-              <div className="form-group mt-4">
-                <div className="d-flex justify-content-between">
-                  <label for="exampleInputPassword1">Choisissez un mot de passe</label>
-                </div>
-                <input
+                <Input
                   type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Entrez votre mot de passe"
+                  label="Mot de passe"
+                  name="password"
+                  id="password"
+                  required={true}
                 />
-              </div>
-              <div className="form-group mt-4">
-                <div className="d-flex justify-content-between">
-                  <label for="exampleInputPhoneNumber1">Votre numéro de téléphone</label>
-                </div>
-
-                <input
-                  type="phoneNumber"
-                  className="form-control"
-                  id="exampleInputPhoneNumber1"
-                  placeholder="Entrez votre numéro de téléphone"
+                <h3 className="topSectionH3 mt-5">Votre profil</h3>
+                <Input
+                  type="text"
+                  label="Prénom"
+                  name="firstname"
+                  id="firstname"
+                  placeholder="ex: Pierre"
+                  required={false}
                 />
-              </div>
-              <div className="form-group mt-4">
-                <div className="d-flex justify-content-between">
-                  <label for="exampleInputCity1">Votre ville</label>
-                </div>
+                <Input
+                  type="text"
+                  label="Nom"
+                  name="lastname"
+                  id="lastname"
+                  placeholder="ex: Dubois"
+                  required={false}
+                />
+                {role === 'shopkeeper' ? <RegisterShopkeeper /> : null}
 
-                <input type="city" className="form-control" id="exampleInputCity1" placeholder="Entrez votre ville" />
-              </div>
-
-              <button type="submit" className="mt-4 btn btn-custom-accent btn-block">
-                Continuer
-              </button>
-            </form>
+                <button type="submit" className="mt-4 btn btn-custom-accent btn-block">
+                  Continuer
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </>
@@ -135,4 +132,3 @@ class Register extends React.Component {
   }
 }
 export default withRouter(Register);
-
