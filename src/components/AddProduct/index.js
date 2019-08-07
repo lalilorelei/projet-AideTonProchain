4405cfa092fb1b6e6aff80d2d96399f19103ec7d
gregvich <gregoshields@hotmail.com>
2019-08-07 11:12:53 +0200

    add-addProduct-static

--- src/components/AddProduct/index.js
+++ src/components/AddProduct/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
 import Header from 'components/Header';
+import Input from 'components/Input';
 import './addProduct.scss';
 
-const AddProduct = () => (
+const AddProduct = ({ submitAddProductForm }) => (
   <>
     <Header title="Ajouter un produit" />
+
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          <form className="mt-4" onSubmit={submitAddProductForm}>
+            <Input label="Nom du produit" type="text" className="form-control" id="name" name="name" required={true} />
+            <Input label="Prix" type="text" className="form-control" id="price" name="price" required={true} />
+            <Input
+              label="Disponibilité"
+              type="number"
+              className="form-control"
+              id="Available"
+              name="available"
+              required={true}
+            />
+            <Input
+              label="Description"
+              type="text"
+              className="form-control"
+              id="description"
+              name="description"
+              required={true}
+            />
+            <button type="submit" className="mt-4 btn btn-custom-accent btn-block">
+              Ajouter un produit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   </>
 );
 
