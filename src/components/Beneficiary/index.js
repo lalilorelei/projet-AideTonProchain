55e55ac8079091f5e347db0bfbfcde4e869ee367
ggglyke <guillaluckyluke@hotmail.fr>
2019-08-13 12:42:50 +0200

    fix listes benef et shopkeepers

--- src/components/Beneficiary/index.js
+++ src/components/Beneficiary/index.js
@@ -1,5 +1,6 @@
 /* React */
 import React from 'react';
+import axios from 'axios';
 
 /* Packages */
 import PropTypes from 'prop-types';
@@ -15,53 +16,126 @@ import Error403 from 'components/Error403';
 /* utils & data */
 import { initGeolocation, geoCode } from 'utils/geolocationUtils';
 import profileSubtitles from 'data/profileSubtitles';
+import defaultAvatar from 'assets/img/default-avatar.png';
 
 /* Css */
 import './beneficiary.scss';
 
 /* medias */
 import shopKeepersBackgroundImage from 'assets/img/background-shopkeepers.jpg';
-import defaultAvatar from 'assets/img/default-avatar.png';
 
 class Beneficiary extends React.Component {
   state = {
-    lat: '',
-    long: '',
+    location: {
+      latitude: 0,
+      longitude: 0,
+    },
     isGeoLocAccessible: true,
     itemsOrderedByDistance: [],
+    beneficiaries: [],
     getLocationErrorMessage: false,
   };
-
   // Avant d'afficher le composant on récupère la localisation via le navigateur et l'ensemble des shops
   componentDidMount = () => {
-    initGeolocation(this);
     document.title = `Personnes à proximité - Aide ton prochain`;
+
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState(
+            {
+              isGeoLocAccessible: true,
+              location: {
+                latitude: Number(position.coords.latitude),
+                longitude: Number(position.coords.longitude),
+              },
+            },
+            () => {
+              const { latitude, longitude } = this.state.location;
+              this.getBeneficiaries(latitude, longitude, 9999);
+            },
+          );
+        },
+        err => {
+          this.setState({
+            isGeoLocAccessible: false,
+          });
+        },
+      );
+    } else {
+      this.setState({
+        isGeoLocAccessible: false,
+      });
+    }
   };
 
-  submitAskLocation = evt => {
+  getBeneficiaries = (latitude, longitude, km) => {
+    axios
+      .post(
+        `http://95.142.175.77:3000/api/${this.props.role}/beneficiaries-distance`,
+        { latitude, longitude, km },
+        {
+          headers: {
+            Authorization: `Bearer ${this.props.token}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          itemsOrderedByDistance: [...response.data.beneficiariesDistance],
+          beneficiaries: [...response.data.beneficiariesDistance],
+        });
+      })
+      .catch(e => {
+        console.log('Impossible de récupérer les beneficiaires', e);
+      });
+  };
+
+  // Soumission du formulaire avec adresse manuell
+  submitAskLocation = async evt => {
     evt.preventDefault();
-    geoCode(this, evt.target.locationAddress.value);
+
+    console.log(evt.target.locationAddress.value);
+    await axios
+      .get(
+        `https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=${
+          evt.target.locationAddress.value
+        }&limit=1`,
+      )
+      .then(response => {
+        const { lat, lon } = response.data[0];
+        this.setState({
+          location: {
+            latitude: Number(lat),
+            longitude: Number(lon),
+          },
+          isGeoLocAccessible: true,
+        });
+        console.log(this.state);
+      })
+      .catch(e => {
+        this.setState({
+          ...this.state,
+          isGeoLocAccessible: false,
+          getLocationErrorMessage: true,
+        });
+      });
+    const { latitude, longitude } = this.state.location;
+    await this.getBeneficiaries(latitude, longitude, 30);
   };
 
   onChangeSelect = evt => {
     const maxDist = evt.target.value;
-    const { location } = this.state;
-    const { getBeneficiaries, role, token } = this.props;
-    getBeneficiaries(role, token, location, maxDist);
+    const { latitude, longitude } = this.state.location;
+    this.getBeneficiaries(latitude, longitude, maxDist);
   };
 
-  componentDidUpdate() {
-    const { beneficiaries, getBeneficiaries, role, token } = this.props;
-    const { location } = this.state;
-
-    if (beneficiaries === undefined && location) {
-      getBeneficiaries(role, token, location, 9999);
-    }
-  }
-
   render() {
-    const { beneficiaries } = this.props;
+    const { beneficiaries } = this.state;
     const { currentUser, role } = this.props;
+    console.log(beneficiaries);
 
     if (currentUser.user !== undefined && role !== 'shopkeeper') {
       return (
@@ -71,7 +145,12 @@ class Beneficiary extends React.Component {
             backgroundImage={shopKeepersBackgroundImage}
             theme="dark"
           />
-          {!this.state.isGeoLocAccessible && (
+          {this.state.isGeoLocAccessible && this.state.location.latitude === 0 ? (
+            <EmptyState
+              className="mt-5"
+              message="Oops, Veuillez nous accorder l'autorisation d'utiliser votre gélocalisation"
+            />
+          ) : !this.state.isGeoLocAccessible ? (
             <div className="container py-5 beneficiary-list">
               <div className="row">
                 <div className="col">
@@ -103,12 +182,11 @@ class Beneficiary extends React.Component {
                 </div>
               </div>
             </div>
-          )}
-          {beneficiaries !== undefined ? (
+          ) : (
             <div className="container py-5 beneficiary-list">
-              <form className="form-inline d-flex justify-content-between">
-                <div className="row my-3">
-                  <div className="col-12">
+              <div className="row my-3">
+                <div className="col-12">
+                  <form className="form-inline d-flex justify-content-between">
                     <div className="form-group">
                       <label className="inline-form-label" htmlFor="exampleFormControlSelect1">
                         Distance maxi :
@@ -117,7 +195,7 @@ class Beneficiary extends React.Component {
                         className="form-control form-control-sm"
                         id="selectDistance"
                         onChange={this.onChangeSelect}
-                        defaultValue={9999}
+                        defaultValue={10000}
                       >
                         <option value="1">1 km</option>
                         <option value="2">2 km</option>
@@ -127,84 +205,76 @@ class Beneficiary extends React.Component {
                         <option value="10">10 km</option>
                         <option value="20">20 km</option>
                         <option value="30">30 km</option>
-                        <option value="9999">Illimitée</option>
+                        <option value="10000">10 000 km</option>
                       </select>
                     </div>
+                  </form>
+                </div>
+              </div>
+              {beneficiaries !== undefined && beneficiaries.length > 0 ? (
+                <div className="row">
+                  {beneficiaries.map(beneficiary => {
+                    return (
+                      <div className="col-12 col-sm-6 col-md-6 col-lg-4 mb-4" key={beneficiary._id}>
+                        <div className="card">
+                          <img
+                            className="card-img-top"
+                            src={
+                              beneficiary.avatar
+                                ? `data:image/jpg;base64,${beneficiary.avatar}`
+                                : defaultAvatar
+                            }
+                            alt={beneficiary.username}
+                          />
+                          <div className="card-body">
+                            <h4 className="card-title mb-0">{beneficiary.username}</h4>
+                            {profileSubtitles.length > 0 && (
+                              <span className="text-muted d-block">
+                                {
+                                  profileSubtitles[
+                                    Math.floor(Math.random() * profileSubtitles.length)
+                                  ]
+                                }
+                              </span>
+                            )}
+
+                            {beneficiary.distance || beneficiary.distance === 0 ? (
+                              <div className="distance card-subtitle mt-3 text-muted text-small d-inline-flex align-items-center">
+                                <FaMapMarkerAlt size=".75rem" color="#bbb" />
+                                <span className="ml-1">{beneficiary.distance} km</span>
+                              </div>
+                            ) : (
+                              <div
+                                className={
+                                  `distance card-subtitle mt-3 text-muted text-small d-inline-flex align-items-center` +
+                                    !beneficiary.distance &&
+                                  ' distance card-subtitle mt-3 text-muted text-small d-inline-flex align-items-center border-none'
+                                }
+                              >
+                                <FaMapMarkerAlt size=".75rem" color="#bbb" />
+                                <span className="ml-1">Non renseigné</span>
+                              </div>
+                            )}
+                          </div>
+                          <Link to={`/beneficiary/${beneficiary._id}`} className="stretched-link" />
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div className="row">
+                  <div className="col">
+                    <EmptyState
+                      className="mt-5"
+                      message="Oops, aucun bénéficiaire n'a été trouvé dans la zone selectionnée"
+                    />
                   </div>
                 </div>
-              </form>
-              <div className="row">
-                {beneficiaries !== false
-                  ? beneficiaries.map(beneficiary => {
-                      return (
-                        <div
-                          className="col-12 col-sm-6 col-md-6 col-lg-4 mb-4"
-                          key={beneficiary._id}
-                        >
-                          <div className="card">
-                            <img
-                              className="card-img-top"
-                              src={
-                                beneficiary.avatar
-                                  ? `data:image/jpg;base64,${beneficiary.avatar}`
-                                  : defaultAvatar
-                              }
-                              alt={beneficiary.username}
-                            />
-                            <div className="card-body">
-                              <h4 className="card-title mb-0">{beneficiary.username}</h4>
-                              {profileSubtitles.length > 0 && (
-                                <span className="text-muted d-block">
-                                  {
-                                    profileSubtitles[
-                                      Math.floor(Math.random() * profileSubtitles.length)
-                                    ]
-                                  }
-                                </span>
-                              )}
-
-                              {beneficiary.distance || beneficiary.distance === 0 ? (
-                                <div className="distance card-subtitle mt-3 text-muted text-small d-inline-flex align-items-center">
-                                  <FaMapMarkerAlt size=".75rem" color="#bbb" />
-                                  <span className="ml-1">{beneficiary.distance} km</span>
-                                </div>
-                              ) : (
-                                <div
-                                  className={
-                                    `distance card-subtitle mt-3 text-muted text-small d-inline-flex align-items-center` +
-                                      !beneficiary.distance &&
-                                    ' distance card-subtitle mt-3 text-muted text-small d-inline-flex align-items-center border-none'
-                                  }
-                                >
-                                  <FaMapMarkerAlt size=".75rem" color="#bbb" />
-                                  <span className="ml-1">Non renseigné</span>
-                                </div>
-                              )}
-                            </div>
-                            <Link
-                              to={`/beneficiary/${beneficiary._id}`}
-                              className="stretched-link"
-                            />
-                          </div>
-                        </div>
-                      );
-                    })
-                  : beneficiaries === false && (
-                      <div className="col">
-                        <EmptyState
-                          className="mt-5"
-                          message="Oops, aucun bénéficiaire n'a été trouvé dans la zone selectionnée"
-                        />
-                      </div>
-                    )}
-              </div>
+              )}
             </div>
-          ) : null}
+          )}
         </>
-      );
-    } else {
-      return (
-        <Error403 message="Vous ne pouvez pas accéder à cette page, vous n'êtes pas connectés" />
       );
     }
   }
--- src/data/testimonies.js
+++ src/data/testimonies.js
@@ -1,20 +1,23 @@
 export default [
-    {
-        name: "Hervé",
-        role: "Donateur",
-        avatar_url: "https://picsum.photos/50/50?var=78",
-        text: "ATP m'a permis de rencontrer des gens magnifiques bien loin des clichés que l'on entend à longueur de journée. J'ai pas mal de temps libre alors je le passe avec les plus démunis et parfois je fais des dons. Très pratique !"
-    },
-    {
-        name: "Gilles",
-        role: "Bénéficiaire",
-        avatar_url: "https://picsum.photos/50/50?var=79",
-        text: "ATP c'est sympa, je rencontre des gens différents mais bienveillants. Parfois on en profite pour discuter autour d'un café acheté sur le site ou qu'on me paye directement au comptoir. Je suis toujours à la rue mais je le vis mieux depuis que je suis inscrit."
-    },
-    {
-        name: "Samia",
-        role: "Commercante",
-        avatar_url: "https://picsum.photos/50/50?var=85",
-        text: "Je cherchais depuis longtemps comment donner un aspect plus solidaire à mon activité. Avec ATP c'était tout trouvé. J'adore le concept. Sur le site je pratique des tarif solidaires."
-    }
+  {
+    name: 'Hervé',
+    role: 'Donateur',
+    avatar_url: 'https://picsum.photos/50/50?var=78',
+    text:
+      "ATP m'a permis de rencontrer des gens magnifiques bien loin des clichés que l'on entend à longueur de journée. J'ai pas mal de temps libre alors je le passe avec les plus démunis et parfois je fais des dons. Très pratique !",
+  },
+  {
+    name: 'Gilles',
+    role: 'Bénéficiaire',
+    avatar_url: 'https://picsum.photos/50/50?var=79',
+    text:
+      "ATP c'est sympa, je rencontre des gens différents mais bienveillants. Parfois on en profite pour discuter autour d'un café acheté sur le site ou qu'on me paye directement au comptoir. Je suis toujours à la rue mais je le vis mieux depuis que je suis inscrit.",
+  },
+  {
+    name: 'Samia',
+    role: 'Commercante',
+    avatar_url: 'https://picsum.photos/50/50?var=85',
+    text:
+      "Je cherchais depuis longtemps comment donner un aspect plus solidaire à mon activité. Avec ATP c'était tout trouvé. J'adore le concept. Sur le site je pratique des tarifs solidaires.",
+  },
 ];
