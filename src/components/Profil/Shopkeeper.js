f22e437efcc0dedbaff3a75d7421549a81d87cd4
ggglyke <guillaluckyluke@hotmail.fr>
2019-08-13 13:44:56 +0200

    fix confirm error update profile

--- src/components/Profil/Shopkeeper.js
+++ src/components/Profil/Shopkeeper.js
@@ -20,11 +20,31 @@ class Shopkeeper extends React.Component {
     }
   };
 
+  openingHours = hour => {
+    let h = '';
+
+    if (hour === 0) {
+      return h;
+    }
+
+    hour = (hour < 10 ? '0' + hour : hour).toString();
+    if (hour.includes('.')) {
+      h = hour.replace('.', 'h');
+      if (h.length === 4) {
+        h = h + '0';
+      }
+    } else {
+      h = hour + 'h00';
+    }
+    return h;
+  };
+
   render() {
     const { role, products } = this.props;
     const shop = this.props.currentUser.user;
 
     const date = new Date();
+    console.log(shop);
 
     return (
       <>
@@ -67,36 +87,138 @@ class Shopkeeper extends React.Component {
                 <tbody>
                   <tr className={date.getDay() === 1 ? 'font-weight-bold' : null}>
                     <td>Lundi</td>
-                    <td className="px-2">10:00 - 17:00</td>
-                    <td>19:00 - 23:00</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.monday.morning_open)}
+                      {this.openingHours(shop.opening_hours.monday.morning_close) !== '' && (
+                        <span> / {this.openingHours(shop.opening_hours.monday.morning_close)}</span>
+                      )}
+                      {' - '}
+                    </td>
+                    {this.openingHours(shop.opening_hours.monday.afternoon_open) !== '' && (
+                      <span>{this.openingHours(shop.opening_hours.monday.afternoon_open)} / </span>
+                    )}
+                    {this.openingHours(shop.opening_hours.monday.afternoon_close)}
+                    <td />
                   </tr>
                   <tr className={date.getDay() === 2 ? 'font-weight-bold' : null}>
                     <td>Mardi</td>
-                    <td className="px-2">10:00 - 17:00</td>
-                    <td>19:00 - 23:00</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.tuesday.morning_open)}{' '}
+                      {this.openingHours(shop.opening_hours.tuesday.morning_close) !== '' && (
+                        <span>
+                          {' '}
+                          / {this.openingHours(shop.opening_hours.tuesday.morning_close)}
+                        </span>
+                      )}
+                      {' - '}
+                    </td>
+                    <td>
+                      {this.openingHours(shop.opening_hours.tuesday.afternoon_open) !== '' && (
+                        <span>
+                          {this.openingHours(shop.opening_hours.tuesday.afternoon_open)} /{' '}
+                        </span>
+                      )}
+                      {this.openingHours(shop.opening_hours.tuesday.afternoon_close)}
+                    </td>
                   </tr>
                   <tr className={date.getDay() === 3 ? 'font-weight-bold' : null}>
                     <td>Mercredi</td>
-                    <td className="px-2">10:00 - 17:00</td>
-                    <td>19:00 - 23:00</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.wednesday.morning_open)}
+                      {this.openingHours(shop.opening_hours.wednesday.morning_close) !== '' && (
+                        <span>
+                          {' '}
+                          / {this.openingHours(shop.opening_hours.wednesday.morning_close)}
+                        </span>
+                      )}
+                      {' - '}
+                    </td>
+                    <td>
+                      {this.openingHours(shop.opening_hours.wednesday.afternoon_open) !== '' && (
+                        <span>
+                          {this.openingHours(shop.opening_hours.wednesday.afternoon_open)} /{' '}
+                        </span>
+                      )}
+                      {this.openingHours(shop.opening_hours.wednesday.afternoon_close)}
+                    </td>
                   </tr>
                   <tr className={date.getDay() === 4 ? 'font-weight-bold' : null}>
                     <td>Jeudi</td>
-                    <td className="px-2">10:00 - 17:00</td>
-                    <td>19:00 - 23:00</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.thursday.morning_open)}
+                      {this.openingHours(shop.opening_hours.thursday.morning_close) !== '' && (
+                        <span>
+                          {' '}
+                          / {this.openingHours(shop.opening_hours.thursday.morning_close)}
+                        </span>
+                      )}
+                      {' - '}
+                    </td>
+                    <td>
+                      {this.openingHours(shop.opening_hours.thursday.afternoon_open) !== '' && (
+                        <span>
+                          {this.openingHours(shop.opening_hours.thursday.afternoon_open)} /{' '}
+                        </span>
+                      )}
+                      {this.openingHours(shop.opening_hours.thursday.afternoon_close)}
+                    </td>
                   </tr>
                   <tr className={date.getDay() === 5 ? 'font-weight-bold' : null}>
                     <td>Vendredi</td>
-                    <td className="px-2">10:00 - 17:00</td>
-                    <td>19:00 - 23:00</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.friday.morning_open)}
+                      {this.openingHours(shop.opening_hours.friday.morning_close) !== '' && (
+                        <span> / {this.openingHours(shop.opening_hours.friday.morning_close)}</span>
+                      )}
+                      {' - '}
+                    </td>
+                    <td>
+                      {this.openingHours(shop.opening_hours.friday.afternoon_open) !== '' && (
+                        <span>
+                          {this.openingHours(shop.opening_hours.friday.afternoon_open)} /{' '}
+                        </span>
+                      )}
+                      {this.openingHours(shop.opening_hours.friday.afternoon_close)}
+                    </td>
                   </tr>
                   <tr className={date.getDay() === 6 ? 'font-weight-bold' : null}>
                     <td>Samedi</td>
-                    <td className="px-2">10:00 - 23:00</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.saturday.morning_open)}
+                      {this.openingHours(shop.opening_hours.saturday.morning_close) !== '' && (
+                        <span>
+                          {' '}
+                          / {this.openingHours(shop.opening_hours.saturday.morning_close)}
+                        </span>
+                      )}
+                      {' - '}
+                    </td>
+                    <td>
+                      {this.openingHours(shop.opening_hours.saturday.afternoon_open) !== '' && (
+                        <span>
+                          {this.openingHours(shop.opening_hours.saturday.afternoon_open)} /{' '}
+                        </span>
+                      )}
+                      {this.openingHours(shop.opening_hours.saturday.afternoon_close)}
+                    </td>
                   </tr>
                   <tr className={date.getDay() === 7 ? 'current-day' : null}>
                     <td>Dimanche</td>
-                    <td className="px-2">Fermé</td>
+                    <td className="px-2">
+                      {this.openingHours(shop.opening_hours.sunday.morning_open)}
+                      {this.openingHours(shop.opening_hours.sunday.morning_close) !== '' && (
+                        <span> / {this.openingHours(shop.opening_hours.sunday.morning_close)}</span>
+                      )}
+                      {' - '}
+                    </td>
+                    <td>
+                      {this.openingHours(shop.opening_hours.sunday.afternoon_open) !== '' && (
+                        <span>
+                          {this.openingHours(shop.opening_hours.sunday.afternoon_open)} /{' '}
+                        </span>
+                      )}
+                      {this.openingHours(shop.opening_hours.sunday.afternoon_close)}
+                    </td>
                   </tr>
                 </tbody>
               </table>
